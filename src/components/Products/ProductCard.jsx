4ef9146cadd71698f70559651b860ba729d5e5ea
Gowtham <gowthamrlmc@gmail.com>
2024-08-25 22:30:59 +0530

    hi

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -3,9 +3,6 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
-
-import "./products.css";
-
 function ProductCard(props) {
   return (
     <Card className="product-card card-shadow card-transparent">
